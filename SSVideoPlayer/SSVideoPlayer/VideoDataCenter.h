0db8ee0f49bdd614dd2a59fb41fc32e757b56634
shimly <575801327@qq.com>
2016-08-16 13:58:52 +0800

    添加MJ

---
+++ SSVideoPlayer/SSVideoPlayer/VideoDataCenter.h
@@ -0,0 +1,13 @@
+//
+//  VideoDataCenter.h
+//  SSVideoPlayer
+//
+//  Created by shimly on 16/8/16.
+//  Copyright © 2016年 shimly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface VideoDataCenter : NSObject
+
+@end
---
+++ SSVideoPlayer/SSVideoPlayer/VideoDataCenter.m
@@ -0,0 +1,13 @@
+//
+//  VideoDataCenter.m
+//  SSVideoPlayer
+//
+//  Created by shimly on 16/8/16.
+//  Copyright © 2016年 shimly. All rights reserved.
+//
+
+#import "VideoDataCenter.h"
+
+@implementation VideoDataCenter
+
+@end
